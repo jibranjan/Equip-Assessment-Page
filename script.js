f630209a6fb1e47ce0d8ef9b8d267070d6d91214
jibranjan <jibranjan77@gmail.com>
2024-01-27 00:30:15 +0530

    loader added to iframes

--- script.js
+++ script.js
@@ -6,50 +6,13 @@ cardMobElArray.forEach(function (cardMobEl) {
   cardMobEl.addEventListener("click", function () {
     let cardMobElId = cardMobEl.getAttribute("id");
     if (activeCardMobId !== cardMobElId) {
-      makePrevCardMobInActive(activeCardMobId);
-      makeNextCardMobActive(cardMobElId);
+      makePrevCardInActive(activeCardMobId, true);
+      makeNextCardActive(cardMobElId, true);
       changeIframe(activeCardMobId, cardMobElId);
       activeCardMobId = cardMobElId;
     }
   });
 });
-
-function makePrevCardMobInActive(previousActiveCardMobId) {
-  let previousActiveCardMobEl = document.getElementById(
-    previousActiveCardMobId
-  );
-
-  previousActiveCardMobEl.classList.remove("border-l-4");
-  let activeSVG = previousActiveCardMobEl.querySelector(".card-mob-icon");
-  activeSVG.classList.add("grayscale");
-
-  let activeCardMobHeading =
-    previousActiveCardMobEl.querySelector(".card-mob-heading ");
-  activeCardMobHeading.classList.add("text-gray-400");
-  activeCardMobHeading.classList.remove("text-blue-900");
-
-  let activeChevron =
-    previousActiveCardMobEl.querySelector(".card-mob-chevron");
-  activeChevron.classList.add("grayscale", "opacity-50");
-  activeChevron.classList.remove("rotate-180");
-}
-
-function makeNextCardMobActive(nextActiveCardMobId) {
-  let nextActiveCardMobEl = document.getElementById(nextActiveCardMobId);
-  nextActiveCardMobEl.classList.add("border-l-4");
-
-  let activeSVG = nextActiveCardMobEl.querySelector(".card-mob-icon");
-  activeSVG.classList.remove("grayscale");
-
-  let activeCardMobHeading =
-    nextActiveCardMobEl.querySelector(".card-mob-heading ");
-  activeCardMobHeading.classList.remove("text-gray-400");
-  activeCardMobHeading.classList.add("text-blue-900");
-
-  let activeChevron = nextActiveCardMobEl.querySelector(".card-mob-chevron");
-  activeChevron.classList.remove("grayscale", "opacity-50");
-  activeChevron.classList.add("rotate-180");
-}
 
 // adding interactivity of Assessment-features section for large devices
 let activeCardIndex = 0;
@@ -70,57 +33,12 @@ cardElArray.forEach(function (cardEl) {
   });
 });
 
-function makePrevCardInActive(previousActiveCardId) {
-  let previousActiveCardEl = document.getElementById(previousActiveCardId);
-  previousActiveCardEl.classList.remove("border-l-4");
-  previousActiveCardEl.classList.add("bg-stone-50");
-
-  let activeSVG = previousActiveCardEl.querySelector(".card-icon");
-  activeSVG.classList.add("grayscale");
-
-  let activeCardHeading = previousActiveCardEl.querySelector(".card-heading ");
-  activeCardHeading.classList.add("text-neutral-400");
-  activeCardHeading.classList.remove("text-blue-900");
-
-  let acticeCardDesc = previousActiveCardEl.querySelector(".card-desc");
-  acticeCardDesc.classList.remove("text-gray-700");
-  acticeCardDesc.classList.add("text-neutral-400");
-}
-
-function makeNextCardActive(nextActiveCardId) {
-  let nextActiveCardEl = document.getElementById(nextActiveCardId);
-  nextActiveCardEl.classList.add("border-l-4");
-  nextActiveCardEl.classList.remove("bg-stone-50");
-
-  let activeSVG = nextActiveCardEl.querySelector(".card-icon");
-  activeSVG.classList.remove("grayscale");
-
-  let activeCardHeading = nextActiveCardEl.querySelector(".card-heading ");
-  activeCardHeading.classList.remove("text-neutral-400");
-  activeCardHeading.classList.add("text-blue-900");
-
-  let acticeCardDesc = nextActiveCardEl.querySelector(".card-desc");
-  acticeCardDesc.classList.add("text-gray-700");
-  acticeCardDesc.classList.remove("text-neutral-400");
-}
-
-// Same for mob and computer
-
-function changeIframe(previousActiveCardId, nextActiveCardId) {
-  let previousActiveOutputId = previousActiveCardId + "-output";
-  let previousActiveOutputEl = document.getElementById(previousActiveOutputId);
-  previousActiveOutputEl.classList.add("hidden");
-  let nextActiveOutputId = nextActiveCardId + "-output";
-  let nextActiveOutputEl = document.getElementById(nextActiveOutputId);
-  nextActiveOutputEl.classList.remove("hidden");
-}
-
 // adding right left chevron effect for assessment features output in large screens
 
 let chevronRightEl = document.getElementById("chevron-right");
 
 chevronRightEl.addEventListener("click", function () {
-  if (activeCardIndex < 6) {
+  if (activeCardIndex < cardElArray.length - 1) {
     let activeCardId = cardElArray[activeCardIndex].getAttribute("id");
     let cardElId = cardElArray[activeCardIndex + 1].getAttribute("id");
     makePrevCardInActive(activeCardId);
@@ -150,12 +68,75 @@ function handleNavButtons(nextIndex) {
     chevronLeftEl.classList.add("opacity-50", "cursor-not-allowed");
     chevronRightEl.classList.remove("opacity-50", "cursor-not-allowed");
   }
-  if (nextIndex > 0 && nextIndex < 6) {
+  if (nextIndex > 0 && nextIndex < cardElArray.length - 1) {
     chevronLeftEl.classList.remove("opacity-50", "cursor-not-allowed");
     chevronRightEl.classList.remove("opacity-50", "cursor-not-allowed");
   }
-  if (nextIndex == 6) {
+  if (nextIndex == cardElArray.length - 1) {
     chevronRightEl.classList.add("opacity-50", "cursor-not-allowed");
     chevronLeftEl.classList.remove("opacity-50", "cursor-not-allowed");
   }
+}
+
+function onIframeLoaded(e) {
+  let loadedIframe = e.target;
+  loadedIframe.classList.remove("hidden");
+  let loaderId = loadedIframe.id + "-loader";
+  let loaderEl = document.getElementById(loaderId);
+  loaderEl.classList.add("hidden");
+}
+
+function makePrevCardInActive(previousActiveCardId, isMobile = false) {
+  let previousActiveCardEl = document.getElementById(previousActiveCardId);
+  previousActiveCardEl.classList.remove("border-l-4");
+
+  let activeSVG = previousActiveCardEl.querySelector(".card-icon");
+  activeSVG.classList.add("grayscale");
+
+  let activeCardHeading = previousActiveCardEl.querySelector(".card-heading ");
+  activeCardHeading.classList.add("text-gray-400");
+  activeCardHeading.classList.remove("text-blue-900");
+
+  if (isMobile) {
+    let activeChevron = previousActiveCardEl.querySelector(".card-mob-chevron");
+    activeChevron.classList.add("grayscale", "opacity-50");
+    activeChevron.classList.remove("rotate-180");
+  } else {
+    previousActiveCardEl.classList.add("bg-stone-50");
+    let acticeCardDesc = previousActiveCardEl.querySelector(".card-desc");
+    acticeCardDesc.classList.remove("text-gray-700");
+    acticeCardDesc.classList.add("text-neutral-400");
+  }
+}
+
+function makeNextCardActive(nextActiveCardId, isMobile = false) {
+  let nextActiveCardEl = document.getElementById(nextActiveCardId);
+  nextActiveCardEl.classList.add("border-l-4");
+
+  let activeSVG = nextActiveCardEl.querySelector(".card-icon");
+  activeSVG.classList.remove("grayscale");
+
+  let activeCardHeading = nextActiveCardEl.querySelector(".card-heading ");
+  activeCardHeading.classList.remove("text-gray-400");
+  activeCardHeading.classList.add("text-blue-900");
+
+  if (isMobile) {
+    let activeChevron = nextActiveCardEl.querySelector(".card-mob-chevron");
+    activeChevron.classList.remove("grayscale", "opacity-50");
+    activeChevron.classList.add("rotate-180");
+  } else {
+    nextActiveCardEl.classList.remove("bg-stone-50");
+    let acticeCardDesc = nextActiveCardEl.querySelector(".card-desc");
+    acticeCardDesc.classList.add("text-gray-700");
+    acticeCardDesc.classList.remove("text-neutral-400");
+  }
+}
+
+function changeIframe(previousActiveCardId, nextActiveCardId) {
+  let previousActiveOutputId = previousActiveCardId + "-output";
+  let previousActiveOutputEl = document.getElementById(previousActiveOutputId);
+  previousActiveOutputEl.classList.add("hidden");
+  let nextActiveOutputId = nextActiveCardId + "-output";
+  let nextActiveOutputEl = document.getElementById(nextActiveOutputId);
+  nextActiveOutputEl.classList.remove("hidden");
 }
